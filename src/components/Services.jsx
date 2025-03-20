eb4c4d4b44d61ea17b9ffc68d04f28bfaa34dee9
SUMIT0110 <122148407+SUMIT0110@users.noreply.github.com>
2025-03-20 15:32:28 +0530

    Update Services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,36 +3,36 @@ import Heading from "./Heading";
 import { motion } from "framer-motion";
 
 const CardBackground = () => (
-  <svg 
-    className="absolute inset-0 w-full h-full opacity-10" 
-    preserveAspectRatio="none" 
-    width="384" 
-    height="366" 
-    viewBox="0 0 384 366" 
-    fill="none" 
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path 
-      vectorEffect="non-scaling-stroke" 
-      d="M32 1H319.453C328.037 1 336.238 4.5601 342.1 10.832L374.648 45.6545C380.015 51.3966 383 58.9629 383 66.8225V334C383 351.121 369.121 365 352 365H32C14.8792 365 1 351.121 1 334V32C1 14.8792 14.8792 1 32 1Z" 
-      stroke="white" 
-      strokeOpacity="0.15" 
-      strokeWidth="2"
-    />
-    <path 
-      vectorEffect="non-scaling-stroke" 
-      d="M32 1H319.453C328.037 1 336.238 4.5601 342.1 10.832L374.648 45.6545C380.015 51.3966 383 58.9629 383 66.8225V334C383 351.121 369.121 365 352 365H32C14.8792 365 1 351.121 1 334V32C1 14.8792 14.8792 1 32 1Z" 
-      stroke="url(#paint0_linear_333_9188)" 
-      strokeOpacity="0.85" 
-      strokeWidth="2"
-    />
-    <defs>
-      <linearGradient id="paint0_linear_333_9188" x1="192" y1="0" x2="192" y2="366" gradientUnits="userSpaceOnUse">
-        <stop stopColor="#33CEFF"/>
-        <stop offset="0.562842" stopColor="#D633FF" stopOpacity="0"/>
-      </linearGradient>
-    </defs>
-  </svg>
+  <div className="absolute inset-0 w-full h-full">
+    <svg 
+      className="w-full h-full opacity-20" 
+      preserveAspectRatio="xMidYMid slice" 
+      viewBox="0 0 384 366" 
+      fill="none" 
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path 
+        vectorEffect="non-scaling-stroke" 
+        d="M32 1H319.453C328.037 1 336.238 4.5601 342.1 10.832L374.648 45.6545C380.015 51.3966 383 58.9629 383 66.8225V334C383 351.121 369.121 365 352 365H32C14.8792 365 1 351.121 1 334V32C1 14.8792 14.8792 1 32 1Z" 
+        stroke="white" 
+        strokeOpacity="0.15" 
+        strokeWidth="2"
+      />
+      <path 
+        vectorEffect="non-scaling-stroke" 
+        d="M32 1H319.453C328.037 1 336.238 4.5601 342.1 10.832L374.648 45.6545C380.015 51.3966 383 58.9629 383 66.8225V334C383 351.121 369.121 365 352 365H32C14.8792 365 1 351.121 1 334V32C1 14.8792 14.8792 1 32 1Z" 
+        stroke="url(#paint0_linear_333_9188)" 
+        strokeOpacity="0.85" 
+        strokeWidth="2"
+      />
+      <defs>
+        <linearGradient id="paint0_linear_333_9188" x1="192" y1="0" x2="192" y2="366" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#33CEFF"/>
+          <stop offset="0.562842" stopColor="#D633FF" stopOpacity="0"/>
+        </linearGradient>
+      </defs>
+    </svg>
+  </div>
 );
 
 const services = [
@@ -84,7 +84,7 @@ const Services = () => {
               initial={{ opacity: 0, y: 20 }}
               whileInView={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: index * 0.1 }}
-              className="relative bg-n-7 rounded-3xl p-6 border border-n-1/10 hover:border-n-1/20 transition-all duration-300 overflow-hidden"
+              className="relative bg-n-7 rounded-3xl p-6 border border-n-1/10 hover:border-n-1/20 transition-all duration-300 overflow-hidden min-h-[300px]"
             >
               <CardBackground />
               
