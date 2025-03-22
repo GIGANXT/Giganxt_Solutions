78f9abf1a6d64cfb166f344129c6e2de5d0037d8
SUMIT0110 <122148407+SUMIT0110@users.noreply.github.com>
2025-03-22 15:04:37 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -33,7 +33,7 @@ const Footer = () => {
               </h4>
               <ul className="list-none mt-4">
                 <li className="font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer mb-4">
-                     <Link to="Pages/About">About Us</Link>
+                     <Link to="/About">About Us</Link>
                 </li>
               </ul>
             </div>
